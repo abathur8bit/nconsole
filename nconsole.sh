5dcaaaed5267a484b2ddd65052e7f2edf887d1eb
Abathur8bit <abathur8bit@8bitcoder.com>
2018-12-08 21:59:22 -0500

    Shell script to make launching easier

---
+++ nconsole.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# $@ - all command line params
+# $1 - first param
+# $# - number of command line params
+java -cp out/production/nconsole -Djava.libraries.path=cmake-build-debug com.axorion.NConsole
