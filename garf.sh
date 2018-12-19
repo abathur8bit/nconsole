7472f2665c8ced58956807894123aee43a7b4071
Abathur8bit <abathur8bit@8bitcoder.com>
2018-12-18 21:20:54 -0500

    updated garfield startup script

--- garf.sh
+++ garf.sh
@@ -2,5 +2,9 @@
 # $@ - all command line params
 # $1 - first param
 # $# - number of command line params
+
+# Run normally
 # java -cp out/production/garfield:out/production/nconsole -Djava.library.path=cmake-build-debug Garfield $@
+
+# Remote debugging since you can't debug a ncurses app in IntelliJ
 java -cp out/production/garfield:out/production/nconsole -Djava.library.path=cmake-build-debug -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 Garfield $@
